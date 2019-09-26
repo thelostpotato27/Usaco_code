37f01bbd2321c6e40df92043d0fa6564d77ac3b7
Jeffrey Chang <thelostpotato27@gmail.com>
2019-09-26 14:12:38 -0700

    USACO coding practice: greedy gift givers
    just doing some practice with for Usaco so I can show Daniel some stuff

--- greedy gift givers/gift.java
+++ greedy gift givers/gift.java
@@ -1,3 +1,22 @@
+/*
+ID: thelost2
+LANG: JAVA
+PROG: gift 1
+*/
+import java.io.*;
+import java.util.*;
+
 class gift{
-    hello world
+    public static void main(String[] args) throws IOException{
+        BufferedReader br = new BufferedReader(new FileReader("gift.in"));
+        PrintWriter pw = new PrintWriter(new FileWriter("gift.out"));
+
+        List<Integer> people = new ArrayList<>();
+        int num = Integer.parseInt(br.readLine());
+
+        pw.print(num);
+        pw.close();
+
+
+    }
 }
