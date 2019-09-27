bf689e1a0cec07251c6012158fc0bd0a658f12eb
Jeffrey Chang <thelostpotato27@gmail.com>
2019-09-27 14:05:09 -0700

    USACO coding practice: greedy gift givers
    trying to figure out how to use a function and also how to buffered read multiple variables.

--- greedy gift givers/gift.java
+++ greedy gift givers/gift.java
@@ -14,9 +14,29 @@ class gift{
         List<Integer> people = new ArrayList<>();
         int num = Integer.parseInt(br.readLine());
 
-        pw.print(num);
+        int[] group = new int[num];
+
+        String[] kids = new String[num];
+
+        for (int i = 0; i < num; i++) {
+            kids[i] = br.readLine().toString();
+        }
+
+        for (int i = 0; i < num; i++) {
+            group[i] = br.readLine().toString();
+            
+            run();
+        }
+
+
+        pw.print(Arrays.toString(kids));
         pw.close();
 
-
+    }
+    public static void run() {
+        
+        for (int i = 0; i < max; i++) {
+            
+        }
     }
 }
