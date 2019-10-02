aad96834f02b1c6013d39912b65a76513d91f740
Jeffrey Chang <thelostpotato27@gmail.com>
2019-10-02 16:23:55 -0700

    Usaco practice: Friday the 13th
    Tried to solve problem of wrong output. There is a proper output now, but the output itself is strange

--- Friday the 13th/friday.java
+++ Friday the 13th/friday.java
@@ -12,53 +12,73 @@ class friday {
         BufferedReader br = new BufferedReader(new FileReader("friday.in"));
         PrintWriter pw = new PrintWriter(new FileWriter("friday.out"));
 
-        int[][] days = new int[7][2];
+        int[] days = new int[7]; // days[0] is saturday
 
-        for (int f = 0; f < 7; f++) { //trying to use a double array to convert leftoverdays into days of the week
-            
-        }
-
-        HashMap<Integer, Integer> months = new HashMap<>();
         int N = Integer.parseInt(br.readLine());
-        months.put(january, 31);
-        months.put(february, 28);
-        months.put(march, 31);
-        months.put(april, 30);
-        months.put(may, 31);
-        months.put(june, 30);
-        months.put(july, 31);
-        months.put(august, 31);
-        months.put(september, 30);
-        months.put(october, 31);
-        months.put(november, 30);
-        months.put(december, 31);
+
+        HashMap<String, Integer> months = new HashMap<>();
+
+        months.put("january", 31);
+        months.put("february", 28);
+        months.put("march", 31);
+        months.put("april", 30);
+        months.put("may", 31);
+        months.put("june", 30);
+        months.put("july", 31);
+        months.put("august", 31);
+        months.put("september", 30);
+        months.put("october", 31);
+        months.put("november", 30);
+        months.put("december", 31);
 
         int start = 1;
         int daysinmonth = 0;
-        int leftover_days = 0;
         for (int i = 0; i < N; i++) {
             int year = 1900 + i;
-            for (String m : months.keyset) {
-                if (m == february) {
+            for (String m : months.keySet()) {
+                if (m == "february") {
                     if (year % 400 == 0) {
                         daysinmonth = 29;
-                        continue;
-                    }
-                    if (year % 100 != 0 && year % 4 == 0) {
+                    } else if (year % 100 != 0 && year % 4 == 0) {
                         daysinmonth = 29;
-                        continue;
+                    } else {
+                        daysinmonth = 28;
                     }
                 }
-                if (m != february){
+
+                if (m != "february") {
                     daysinmonth = months.get(m);
                 }
+
+                if (start == 7) {
+                    days[0] += 1;
+                }
+                if (start == 6) {
+                    days[6] += 1;
+                }
+                if (start == 5) {
+                    days[5] += 1;
+                }
+                if (start == 4) {
+                    days[4] += 1;
+                }
+                if (start == 3) {
+                    days[3] += 1;
+                }
+                if (start == 2) {
+                    days[2] += 1;
+                }
+                if (start == 1) {
+                    days[1] += 1;
+                }
+                start = ((daysinmonth - (start - 1)) % 7) + 1;
             }
-            if (start == 7){
-                friday13_count += 1;
-            }
-            leftover_days = ((daysinmonth + start) % 7) + 1;
         }
-        
+
+        for (int i = 0; i < 7; i++) {
+            pw.print(days[i] + " ");
+        }
         pw.close();
     }
+
 }
